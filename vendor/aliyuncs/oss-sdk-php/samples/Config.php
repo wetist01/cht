184d217132c9fb79f14365a7b5a4ca55335d02d5
wetist <evankong@wetist.com>
2017-01-24 15:47:45 +0800

    vendor

---
+++ vendor/aliyuncs/oss-sdk-php/samples/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Class Config
+ *
+ * 执行Sample示例所需要的配置，用户在这里配置好Endpoint，AccessId， AccessKey和Sample示例操作的
+ * bucket后，便可以直接运行RunAll.php, 运行所有的samples
+ */
+final class Config
+{
+    const OSS_ACCESS_ID = 'LTAIlOe2RKt0Bh6R';
+    const OSS_ACCESS_KEY = 'lBD3PKPjIZ6k9ZCZjdJAg8fDVovBdZ';
+    const OSS_ENDPOINT = 'oss-cn-shanghai.aliyuncs.com';
+    const OSS_TEST_BUCKET = 'nothave-test';
+}
