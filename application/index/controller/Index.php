7a7273d24ba7da87c7dcb46c95db4efaa42b9b15
wetist <evankong@wetist.com>
2017-09-08 02:35:01 +0800

    热更新

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -22,4 +22,12 @@ class Index extends Base
         $show = Env::get('show');
         return json($show);
     }
+
+    function gonggao()
+    {
+        $text = 'i传话筒隶属于上海呆马科技有限公司，传话筒顾名思义就是给大家传递信息，我们与部分高校合作，定期发布通知信息。上海呆马信息科技公司
+        成立于2016年9月，注册地上海宝山，若在使用本产品的过程中有什么意见或建议请告知我们。
+        联系电话：15651079118';
+        return json($text);
+    }
 }
