191654fb408bcfef662ad6b634d6f9364b5f4b50
wetist <evankong@wetist.com>
2017-09-08 01:28:05 +0800

    热更新

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,17 +1,15 @@
 <?php
+
 namespace app\index\controller;
 
-use app\index\model\User;
-use think\Cache;
 use think\Db;
-use think\helper\Time;
 use think\Request;
 
 class Index extends Base
 {
     public function _initialize($token_allow = [], $request = null)
     {
-        $token_allow = ['upload_img_head'];//需要token验证的action,小写
+        $token_allow = [];//需要token验证的action,小写
         parent::_initialize($token_allow, $request);
     }
 
@@ -20,30 +18,8 @@ class Index extends Base
         return view('index');
     }
 
-    function test()
+    function show()
     {
-        return view('test');
+        return json(1);
     }
-
-    //上传头像接口
-    public function upload_img_head()
-    {
-        $request = Request::instance();
-        $file = $request->file('image');
-        $uid = $request->param('uid', '');
-        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads' . DS . 'head');
-        if ($info) {
-            $extension = $info->getSaveName();
-            $bucket = 'cht-img';
-            $object = 'head/' . $extension;
-            $file = 'uploads/head/' . $extension;
-            if (upload_file_oss($bucket, $object, $file)) {
-                Db::table('cht_user')->where('uid', $uid)->setField('img_head', 'http://img.chuanhuatong.cc/' . $object);
-                data_format_json(0, ['url' => 'http://img.chuanhuatong.cc/' . $object], '上传成功');
-            }
-        } else {
-            echo $file->getError();
-        }
-    }
-
 }
