5b4e9f4e57ecd99bc3a7227ee16834459b214d15
wetist <evankong@wetist.com>
2017-09-08 00:21:52 +0800

    更改缓存时间

--- application/index/config.php
+++ application/index/config.php
@@ -3,4 +3,4 @@ define('ACCESS_KEY_ID', 'LTAIlOe2RKt0Bh6R');
 define('ACCESS_KEY_SECRET', 'lBD3PKPjIZ6k9ZCZjdJAg8fDVovBdZ');
 define('ENDPOINT', 'oss-cn-shanghai.aliyuncs.com');
 define("INTERFACE_KEY", "f1a66a11f9cd5f1d2f4ecb54cf37wwn8");//加密token的key
-define("REDIS_EXPIRE_TIME_TOKEN", 3600*24*30);//token失效时间 TODO 根据实际情况修改
+define("REDIS_EXPIRE_TIME_TOKEN", 3600 * 24);//token失效时间 TODO 根据实际情况修改
