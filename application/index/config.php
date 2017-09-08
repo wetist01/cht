1fa5f1bab978d6194f34efca32e4f262f1556741
wetist <evankong@wetist.com>
2017-09-09 03:55:06 +0800

    修改oss上传地址

--- application/index/config.php
+++ application/index/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ACCESS_KEY_ID', 'LTAIlOe2RKt0Bh6R');
 define('ACCESS_KEY_SECRET', 'lBD3PKPjIZ6k9ZCZjdJAg8fDVovBdZ');
-define('ENDPOINT', 'oss-cn-shanghai.aliyuncs.com');
+define('ENDPOINT', 'oss-cn-shanghai-internal.aliyuncs.com');
 define("INTERFACE_KEY", "f1a66a11f9cd5f1d2f4ecb54cf37wwn8");//加密token的key
 define("REDIS_EXPIRE_TIME_TOKEN", 3600 * 24);//token失效时间 TODO 根据实际情况修改
