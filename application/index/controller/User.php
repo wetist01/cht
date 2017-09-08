2dfc557227ac912c7e3f4f9e4c689ec37c17fe05
wetist <evankong@wetist.com>
2017-09-08 08:18:16 +0800

    模板消息

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -17,7 +17,7 @@ class User extends Base
 {
     public function _initialize($token_allow = [], $request = null)
     {
-        $token_allow = ['logout', 'upload_img_head', 'user_index_init', 'profileedit', 'templatenotice'];//需要token验证的action,小写
+        $token_allow = ['logout', 'upload_img_head', 'user_index_init', 'profileedit'];//需要token验证的action,小写
         parent::_initialize($token_allow, $request);
     }
 
