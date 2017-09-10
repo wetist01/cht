db044df4ef6095ae0bd6f462f78e2c1941dedb3a
wetist <evankong@wetist.com>
2017-09-11 06:24:43 +0800

    1

--- application/index/service/Notice.php
+++ application/index/service/Notice.php
@@ -37,6 +37,7 @@ class Notice extends Base
 
         $access_token = wxapp_access_token();
         $url = 'https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=' . $access_token;
+        Cache::set('test',$data);
         $data = json_encode($data);
         data_format_json(http_post($url, $data));
     }
