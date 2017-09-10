5cbf4c4263a551283216a0ef3990944fc50b22b0
wetist <evankong@wetist.com>
2017-09-11 06:08:39 +0800

    1

--- application/index/service/User.php
+++ application/index/service/User.php
@@ -178,14 +178,12 @@ class User extends Base
             'data' => [
                 '评论内容' => $content,
                 '评论时间' => date('Y-m-d H:i', time())
-            ],
+            ]
         ];
 
         $access_token = wxapp_access_token();
         $url = 'https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=' . $access_token;
         $data = json_encode($data);
-        dump($data);
-        exit;
         data_format_json(http_post($url, $data));
     }
 
