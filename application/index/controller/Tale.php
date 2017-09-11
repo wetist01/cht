bcf02eff43c152494e3a2221116e0a26c1595102
wetist <evankong@wetist.com>
2017-09-11 16:16:01 +0800

    1

--- application/index/controller/Tale.php
+++ application/index/controller/Tale.php
@@ -28,13 +28,8 @@ class Tale extends Base
             $uid = $request->param('uid', 0, 'intval');
             $token = $request->param('token', '');
             $version = $request->param('version', '');
-            if ($version == '1.0.2.0') {
-                $long = 118.9015800;
-                $lat = 31.9126590;
-            } else {
-                $long = $request->param('longitude', null, 'floatval') or data_format_json(-5, '', 'longitude is null');
-                $lat = $request->param('latitude', null, 'floatval') or data_format_json(-6, '', 'latitude is null');
-            }
+            $long = $request->param('longitude', null, 'floatval') or data_format_json(-5, '', 'longitude is null');
+            $lat = $request->param('latitude', null, 'floatval') or data_format_json(-6, '', 'latitude is null');
             $page = $request->param('page', 1, 'positive_intval');
             $near_error = $request->param('near_error', 6, 'intval');//定位范围误差值，6代表2km内
             $service_tale = new \app\index\service\Tale();
@@ -48,89 +43,89 @@ class Tale extends Base
         }
     }
 
-    /**
-     * 热门
-     */
-    function taleListHot()//TODO
-    {
-        $this->taleList();
-    }
+/**
+ * 热门
+ */
+function taleListHot()//TODO
+{
+    $this->taleList();
+}
 
-    //获取单条吐槽详情
-    function taleInfo()
-    {
-        $request = Request::instance();
-        $data['tale_id'] = $request->param('tale_id', 0, 'intval') or data_format_json(-1, '', 'tale_id is null');
-        $data['longitude'] = $request->param('longitude', null, 'floatval') or data_format_json(-1, '', 'longitude is null');
-        $data['latitude'] = $request->param('latitude', null, 'floatval') or data_format_json(-1, '', 'latitude is null');
-        $service_tale = new \app\index\service\Tale();
-        $service_tale->get_tale_info($data);
-    }
+//获取单条吐槽详情
+function taleInfo()
+{
+    $request = Request::instance();
+    $data['tale_id'] = $request->param('tale_id', 0, 'intval') or data_format_json(-1, '', 'tale_id is null');
+    $data['longitude'] = $request->param('longitude', null, 'floatval') or data_format_json(-1, '', 'longitude is null');
+    $data['latitude'] = $request->param('latitude', null, 'floatval') or data_format_json(-1, '', 'latitude is null');
+    $service_tale = new \app\index\service\Tale();
+    $service_tale->get_tale_info($data);
+}
 
-    //创建吐槽接口
-    function createTale()
-    {
-        $request = Request::instance();
-        if ($request->isAjax() || $request->isGet() || $request->isPost()) {//TODO
-            $data['uid'] = $request->param('uid', 0, 'intval');
-            $data['form_id'] = $request->param('form_id');
-            $data['longitude'] = $request->param('longitude', null, 'floatval') or data_format_json(-5, '', 'longitude is null');
-            $data['latitude'] = $request->param('latitude', null, 'floatval') or data_format_json(-6, '', 'latitude is null');
-            $data['description'] = $request->param('description', '') or data_format_json(-1, '', 'description is null');
-            $data['is_anon'] = $request->param('is_anon', 0, 'intval');
-            $data['type'] = $request->param('type', null, 'intval') or data_format_json(-7, '', 'type is null');
-            if ($data['type'] == 2) {
-                $data['img'] = $request->param('img');
-            }
-            $service_tale = new \app\index\service\Tale();
-            $service_tale->create_tale($data);
+//创建吐槽接口
+function createTale()
+{
+    $request = Request::instance();
+    if ($request->isAjax() || $request->isGet() || $request->isPost()) {//TODO
+        $data['uid'] = $request->param('uid', 0, 'intval');
+        $data['form_id'] = $request->param('form_id');
+        $data['longitude'] = $request->param('longitude', null, 'floatval') or data_format_json(-5, '', 'longitude is null');
+        $data['latitude'] = $request->param('latitude', null, 'floatval') or data_format_json(-6, '', 'latitude is null');
+        $data['description'] = $request->param('description', '') or data_format_json(-1, '', 'description is null');
+        $data['is_anon'] = $request->param('is_anon', 0, 'intval');
+        $data['type'] = $request->param('type', null, 'intval') or data_format_json(-7, '', 'type is null');
+        if ($data['type'] == 2) {
+            $data['img'] = $request->param('img');
         }
-    }
-
-    /**
-     * 我发布的
-     */
-    function myTaleList()
-    {
-        $request = Request::instance();
-        $uid = $request->param('uid', 0, 'intval') or data_format_json(-1, '', 'uid is null');
-        $page = $request->param('page', 1, 'positive_intval');
-        $long = $request->param('longitude', null, 'floatval') or data_format_json(-1, '', 'longitude is null');
-        $lat = $request->param('latitude', null, 'floatval') or data_format_json(-1, '', 'latitude is null');
         $service_tale = new \app\index\service\Tale();
-        $service_tale->my_tale_list($uid, $page, $long, $lat);
+        $service_tale->create_tale($data);
     }
+}
 
-    //删除吐槽
-    function deleteTale()//TODO
-    {
-        $request = Request::instance();
-        $where['uid'] = $request->param('uid', 0, 'intval');
-        $where['tale_id'] = $request->param('tale_id', 0, 'intval');
-        $service_tale = new \app\index\service\Tale();
-        $service_tale->delete_tale($where);
-    }
+/**
+ * 我发布的
+ */
+function myTaleList()
+{
+    $request = Request::instance();
+    $uid = $request->param('uid', 0, 'intval') or data_format_json(-1, '', 'uid is null');
+    $page = $request->param('page', 1, 'positive_intval');
+    $long = $request->param('longitude', null, 'floatval') or data_format_json(-1, '', 'longitude is null');
+    $lat = $request->param('latitude', null, 'floatval') or data_format_json(-1, '', 'latitude is null');
+    $service_tale = new \app\index\service\Tale();
+    $service_tale->my_tale_list($uid, $page, $long, $lat);
+}
 
-    //吐槽图片上传
-    function upload_img()//TODO
-    {
-        $request = Request::instance();
-        $file = $request->file('image');
-        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads' . DS . 'tale');
-        if ($info) {
-            $extension = $info->getSaveName();
-            $bucket = 'cht-img';
-            $object = 'tale/' . $extension;
-            $file = 'uploads/tale/' . $extension;
-            if (upload_file_oss($bucket, $object, $file)) {
-                $res = [
-                    'image_url' => 'http://img.chuanhuatong.cc/' . $object
-                ];
-                data_format_json(0, $res, '上传成功');
-            }
-        } else {
-            data_format_json(-100, $file->getError(), '服务异常');
+//删除吐槽
+function deleteTale()//TODO
+{
+    $request = Request::instance();
+    $where['uid'] = $request->param('uid', 0, 'intval');
+    $where['tale_id'] = $request->param('tale_id', 0, 'intval');
+    $service_tale = new \app\index\service\Tale();
+    $service_tale->delete_tale($where);
+}
+
+//吐槽图片上传
+function upload_img()//TODO
+{
+    $request = Request::instance();
+    $file = $request->file('image');
+    $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads' . DS . 'tale');
+    if ($info) {
+        $extension = $info->getSaveName();
+        $bucket = 'cht-img';
+        $object = 'tale/' . $extension;
+        $file = 'uploads/tale/' . $extension;
+        if (upload_file_oss($bucket, $object, $file)) {
+            $res = [
+                'image_url' => 'http://img.chuanhuatong.cc/' . $object
+            ];
+            data_format_json(0, $res, '上传成功');
         }
+    } else {
+        data_format_json(-100, $file->getError(), '服务异常');
     }
+}
 
 }
