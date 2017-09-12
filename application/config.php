0a99ee667ea334cc989d76ce1fd685f6976e3c59
wetist <evankong@wetist.com>
2017-09-13 05:07:33 +0800

    test

--- application/config.php
+++ application/config.php
@@ -191,7 +191,7 @@ return [
         // 缓存有效期 0表示永久缓存
         'expire' => 86400 * 30,
         // redis 服务器
-        'host' => \think\Env::get('redis.host','106.14.6.2033'),
+        'host' => \think\Env::get('redis.host','106.14.6.203'),
         // 密码
         'password'   => 'kongjian2017',
         // redis 服务器端口
@@ -213,7 +213,7 @@ return [
         // 是否自动开启 SESSION
         'auto_start'     => true,
         //host
-        'host'       => \think\Env::get('redis.host','106.14.6.2033'),
+        'host'       => \think\Env::get('redis.host','106.14.6.203'),
         // redis端口
         'port'       => 6379,
         // 密码
